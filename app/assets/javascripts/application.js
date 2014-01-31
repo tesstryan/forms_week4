75a523154770afa63488789376f06408f9d7810a
tesstryan <tesstryan@gmail.com>
2014-01-31 14:43:47 -0600

    Added Show password and took password character requirements off

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,5 +30,28 @@
 
 // });
 
+$(function(){
+    $(".showpassword").each(function(index,input) {
+        var $input = $(input);
+        $('<label class="showpasswordlabel"/>').append(
+            $("<input type='checkbox' class='showpasswordcheckbox' />").click(function() {
+                var change = $(this).is(":checked") ? "text" : "password";
+                var rep = $("<input type='" + change + "' />")
+                    .attr("id", $input.attr("id"))
+                    .attr("name", $input.attr("name"))
+                    .attr('class', $input.attr('class'))
+                    .val($input.val())
+                    .insertBefore($input);
+                $input.remove();
+                $input = rep;
+             })
+        ).append($("<span/>").text(" Show password")).insertAfter($input);
+    });
+});
 
-$(function(){ $(document).foundation(); });
+
+$(function() { 
+	$(document).foundation(); 
+
+
+});
